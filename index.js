81967d3dae4afa5cb433801f631a191b0fc5c018
Caduzinhok <carlos_fire1@outlook.com>
2024-02-16 21:18:21 -0300

    refactor: comment js and adjust spacing lines

--- index.js
+++ index.js
@@ -8,20 +8,22 @@ for (let i = 0; i < items.length; i++) {
     tab.push(items[i].innerHTML);
 }
 
-// get selected li into text fields and get the index
 function resetColorSelect(items){
     for (let i = 0; i < items.length; i++) {
+        // Set all styles on li to default 
         items[i].style.backgroundColor = null;
         items[i].style.color = null;
     }
 }
 
+// get selected li index
 for (let i = 0; i < items.length; i++) {
     items[i].onclick = function () {
         resetColorSelect(items);
 
-        valueResult = tab.indexOf(this.innerHTML) + 1;
+        valueResult = tab.indexOf(this.innerHTML) + 1; // Get the value of li
         
+        // Set the li selected to different style
         items[i].style.backgroundColor = "hsl(217, 12%, 63%)";
         items[i].style.color = "hsl(0, 0%, 100%)";
     };
